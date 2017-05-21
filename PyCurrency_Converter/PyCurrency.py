ca2650e7fce1a823ebc39b8b5274bff52d363278
Python3pkg <raliclo@gmail.com>
2017-05-21 13:42:56 -0700

    Convert to Python3

--- PyCurrency_Converter/PyCurrency.py
+++ PyCurrency_Converter/PyCurrency.py
@@ -17,7 +17,7 @@
 try:
     import urllib.request as urllib2
 except ImportError:
-    import urllib2
+    import urllib.request, urllib.error, urllib.parse
 try:
     from bs4 import BeautifulSoup
 except ImportError:
@@ -28,7 +28,7 @@ class PyCurrency:
     @staticmethod
     def convert(amount, _from, _to):
         url = 'https://www.google.com/finance/converter?a={}&from={}&to={}'.format(amount, _from, _to)
-        response = urllib2.urlopen(url)
+        response = urllib.request.urlopen(url)
         html = response.read()
         parsed = BeautifulSoup(html, 'lxml').body.find('span', attrs={'class': 'bld'}).text
         return parsed
@@ -36,12 +36,12 @@ class PyCurrency:
     @staticmethod
     def codes():
         url = 'https://www.google.com/finance/converter'
-        response = urllib2.urlopen(url)
+        response = urllib.request.urlopen(url)
         html = response.read()
         parser = BeautifulSoup(html, 'lxml').body.find_all('option')
         for code in parser:
             code = str(code).strip().split('<')[1]
-            print(code.strip().split('>')[-1])
+            print((code.strip().split('>')[-1]))
 
 
 def convert(amount, _from, _to):
